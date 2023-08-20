c262fd58e047be8cc6197da4c0f0bdcc10be97bd
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2023-08-20 17:46:21 +0530

    Update BinaryConversion.java

--- Task 1/BinaryConversion.java
+++ Task 1/BinaryConversion.java
@@ -5,18 +5,18 @@ class BinaryConversion
   {
     Scanner sc = new Scanner(System.in);
     System.out.println("*** Binary Conversion ***");
-    System.out.print("Emter a Decimal Number");
-    int n = sc.nextInt();
-    int temp = n;
-    String mul ="";
+    System.out.print("Emter a Decimal Number:");
+    int num = sc.nextInt();
+    int temp = num;
+    String bin ="";
     int i =0;
     while(temp!=0)
     {
       int r =temp%2;
-      mul = r + mul;
+      bin = r + bin;
       temp =temp/2;
     }
-    System.out.print("Binary number of "+n+" is "+mul);
+    System.out.print("Binary number of "+num+" is "+bin);
 
   }
 }
