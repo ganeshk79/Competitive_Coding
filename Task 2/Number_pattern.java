96b78c700ce9d188803e84d77c78be81efc64b43
ganeshk79 <138688125+ganeshk79@users.noreply.github.com>
2023-08-20 21:53:53 +0530

    Update Number_pattern.java

--- Task 2/Number_pattern.java
+++ Task 2/Number_pattern.java
@@ -2,7 +2,8 @@ class Number_pattern
   {
     public static void main(String args[])
     {
-      for(int i =1;i <6;i++)
+      int n =6;
+      for(int i =1;i <n;i++)
         {
           for(int j =0;j<i;j++)
             {
